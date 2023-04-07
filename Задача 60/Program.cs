c8d930992aac451e56188097f3dad8c839467f12
Stas <www.stas1996@mail.ru>
2023-04-07 12:56:41 +0300

    Задача 60

---
+++ Задача 60/Program.cs
@@ -0,0 +1,37 @@
+﻿void ImputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int[,,] matrix = new int[2, 2, 2];
+
+
+Console.Clear();
+ImputMatrix(matrix);
+PrintMatrix(matrix);
+
+
+
