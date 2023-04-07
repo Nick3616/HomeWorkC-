984def5823a4448c29b20db4031e02284a98426d
Stas <www.stas1996@mail.ru>
2023-04-07 12:56:32 +0300

    Задача 58

---
+++ Задача 58/Program.cs
@@ -0,0 +1,54 @@
+﻿void ImputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int[4, 4];
+int[,] matrix1 = new int[4, 4];
+int[,] rez = new int[4, 4];
+int sum = 0;
+
+void Prozmatrix(int[,] rez)
+{
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int per = (matrix[i, j] * matrix1[j, k]);
+                sum += per;
+            }
+            rez[i, k] = sum;
+            sum = 0;
+        }
+    }
+}
+Console.Clear();
+ImputMatrix(matrix);
+ImputMatrix(matrix1);
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix1);
+Console.WriteLine();
+Prozmatrix(rez);
+PrintMatrix(rez);
+
+
